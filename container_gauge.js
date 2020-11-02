e97060bda87c561bb1c4b42a80f364e628c2104e
imsanjoykb <sanjoy.eee32@gmail.com>
2020-11-02 19:34:12 +0600

    Gauge

---
+++ container_gauge.js
@@ -0,0 +1,120 @@
+Highcharts.chart('container_gauge', {
+
+  chart: {
+    type: 'gauge',
+    plotBackgroundColor: null,
+    plotBackgroundImage: null,
+    plotBorderWidth: 0,
+    plotShadow: false
+  },
+
+  title: {
+    text: ''
+  },
+
+  pane: {
+    startAngle: -150,
+    endAngle: 150,
+    background: [{
+      backgroundColor: {
+        linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+        stops: [
+          [0, '#FFF'],
+          [1, '#333']
+        ]
+      },
+      borderWidth: 0,
+      outerRadius: '109%'
+    }, {
+      backgroundColor: {
+        linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+        stops: [
+          [0, '#333'],
+          [1, '#FFF']
+        ]
+      },
+      borderWidth: 1,
+      outerRadius: '107%'
+    }, {
+      // default background
+    }, {
+      backgroundColor: '#DDD',
+      borderWidth: 0,
+      outerRadius: '105%',
+      innerRadius: '103%'
+    }]
+  },
+
+  // the value axis
+  yAxis: {
+    min: 0,
+    max: 1297977,
+
+    minorTickInterval: 'auto',
+    minorTickWidth: 1,
+    minorTickLength: 10,
+    minorTickPosition: 'inside',
+    minorTickColor: '#666',
+
+    tickPixelInterval: 30,
+    tickWidth: 2,
+    tickPosition: 'inside',
+    tickLength: 10,
+    tickColor: '#666',
+    labels: {
+      step: 2,
+      rotation: 'auto'
+    },
+    title: {
+      text: 'threat/hr'
+    },
+    plotBands: [{
+      from: 0,
+      to: 259595,
+      color: '#55BF3B' // green
+    }, {
+      from: 259595,
+      to: 519191,
+      color: '#55BF3B' // green
+    }, {
+      from: 519191,
+      to: 778786,
+      color: '#55BF3B' // green
+    }, {
+      from: 778786,
+      to: 1038382,
+      color: '#55BF3B' // green
+    }, {
+      from: 1038382,
+      to: 1297977,
+      color: '#55BF3B' // green
+    }]
+  },
+
+  series: [{
+    name: 'Threat',
+    data: [80],
+    tooltip: {
+      valueSuffix: 'threat/hr'
+    }
+  }]
+
+},
+// Add some life
+function (chart) {
+  if (!chart.renderer.forExport) {
+    setInterval(function () {
+      var point = chart.series[0].points[0],
+        newVal,
+        inc = Math.round((Math.random() - 0.5) * 20);
+
+      newVal= 19;
+      if (newVal < 0 || newVal > 200) {
+        newVal= 19;
+      }
+
+      point.update(newVal);
+
+    }, 3000);
+  }
+});
